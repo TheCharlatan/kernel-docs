b54a1df013e27ec96ca8e5c7d492a9e1d6aed0d1
TheCharlatan <seb.kung@gmail.com>
2025-07-28 21:05:38 +0200

    Update to latest API

--- docs/search/defines_0.js
+++ docs/search/defines_0.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['bitcoinkernel_5fapi_0',['BITCOINKERNEL_API',['../bitcoinkernel_8h.html#a012147c9e39e35558753fca94f091e26',1,'bitcoinkernel.h']]],
   ['bitcoinkernel_5farg_5fnonnull_1',['BITCOINKERNEL_ARG_NONNULL',['../bitcoinkernel_8h.html#a70490d496006ecc54b23662de7d5b413',1,'bitcoinkernel.h']]],
-  ['bitcoinkernel_5fgnuc_5fprereq_2',['BITCOINKERNEL_GNUC_PREREQ',['../bitcoinkernel_8h.html#a8bfa3290918ac6f668eec68fa30f2428',1,'bitcoinkernel.h']]],
-  ['bitcoinkernel_5fwarn_5funused_5fresult_3',['BITCOINKERNEL_WARN_UNUSED_RESULT',['../bitcoinkernel_8h.html#a259ab885452a8081ca971f8804489930',1,'bitcoinkernel.h']]]
+  ['bitcoinkernel_5fwarn_5funused_5fresult_2',['BITCOINKERNEL_WARN_UNUSED_RESULT',['../bitcoinkernel_8h.html#a259ab885452a8081ca971f8804489930',1,'bitcoinkernel.h']]]
 ];
